fcba4e9f8becddb4349950be1ae242f44cda15d6
dirk122119 <dirk122119@gmail.com>
2023-06-28 20:52:57 +0800

    fix icon

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,10 @@
+import { Metadata } from 'next'
 import Banner from '@/components/Banner'
 import BlogPostSection from '@/components/BlogPostSection'
 import CoachIntroductionSection from '@/components/CoachIntroductionSection'
 import VideoDirPostSection from '@/components/VideoDirPostSection'
+
+
 
 export default function Home() {
   return (
